c6739224bfcfa108ae1e2b7f5bb19f208030ad8a
cej25 <cjones@gsi.de>
2023-10-20 14:08:19 +0200

    MADC32 structure header

---
+++ BB7_MADC_Detector_System.cxx
@@ -0,0 +1,8 @@
+#include "BB7_MADC_Detector_System.h"
+
+#include <cstdlib>
+#include <map>
+
+#include "MADC32.h"
+#include "Configuration_Files/DESPEC_General_Setup/DESPEC_Setup_File.h"
+
---
+++ MADC32.h
@@ -0,0 +1,16 @@
+#ifndef MADC32_H
+#define MADC32_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+struct MADC_Header
+{
+    stuff
+};
+
+
+
+
+#endif
